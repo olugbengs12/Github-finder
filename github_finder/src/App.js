efda0f3c4dc9fbc9cd2ca41a1fe772b3edb35a2c
Gbenga <owoeyegbengulo@gmail.com>
2020-04-03 13:00:26 +0100

    building the navbar

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
 
   render(){
     return (
-      <div className='App'>
-        <h1>Hello from Learn React</h1>
-        <h2>We are Live</h2>
-      </div>
+      <nav className='navbar bg-primary'>
+       <Navbar />
+      </nav>
     );
   }
-  
+
 }
 
 export default App;
