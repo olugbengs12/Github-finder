db55aaa1025d5eeb3e8763ae122279c59d88e7b6
Gbenga <owoeyegbengulo@gmail.com>
2020-04-04 10:14:31 +0100

    moved to functional component

--- github_finder/src/components/users/UserItem.js
+++ github_finder/src/components/users/UserItem.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
-
-class UserItem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo"
-  };
-  render() {
-    const { id, login, avatar_url, html_url } = this.state;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+import React from "react";
+import PropTypes from "prop-types";
+const UserItem = ({ user: { id, login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
+
+UserItem.prototypes = {
+  user: PropTypes.object.isRequired
+};
