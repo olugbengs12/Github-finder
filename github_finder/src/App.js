e87280de9c3bb2c7a949f886978b9bebda3dc52a
Gbenga <gbengulo005@gmail.com>
2020-04-11 17:56:27 +0100

    added a timeout function to alert

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -35,6 +36,8 @@ class App extends Component {
   //Alert User
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
@@ -43,6 +46,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
