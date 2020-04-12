3f89a8db5b418d645d35710b3b7f43c102810301
Gbenga <gbengulo005@gmail.com>
2020-04-12 14:48:13 +0100

    used router to make an about page

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -64,7 +65,8 @@ class App extends Component {
                     <Users loading={loading} users={users} />
                   </Fragment>
                 )}
-              ></Route>
+              />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
         </div>
