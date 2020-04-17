3c980af29c1889d4339ab516681c33be169f0f24
Gbenga <gbengulo005@gmail.com>
2020-04-17 19:57:34 +0100

    refactoring code base by using reducer

---
+++ github_finder/src/context/github/GithubState.js
@@ -0,0 +1,45 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import GithubContext from "./githubContext";
+import GithubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  CLEAR_USERS,
+  SET_LOADING,
+  GET_REPOS,
+  GET_USERS
+} from "../types";
+
+const GithubState = props => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  };
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  //Search Users
+
+  //Clear Users
+
+  //Set Loading
+
+  //Get Repos
+
+  //Get Users
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+export default GithubState;
