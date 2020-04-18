8ce58982d9b650e3cc0e44b8ddc111fa524b1e56
Gbenga <gbengulo005@gmail.com>
2020-04-18 20:53:13 +0100

    refactoring

--- github_finder/src/components/pages/About.js
+++ github_finder/src/components/pages/About.js
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <Fragment>
       <h1>About this App</h1>
-      <p>App to search GitHub Users made with love from Gbenga</p>
+      <p>
+        App to search GitHub Users made with love from{" "}
+        <a href="https://github.com/olugbengs12">Gbenga</a>
+      </p>
       <p>Version: 1.0.0</p>
     </Fragment>
   );
