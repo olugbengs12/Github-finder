9be5d1e1f1ef769edde73fc84347e8fb16d8654e
Gbenga <gbengulo005@gmail.com>
2020-04-18 21:23:03 +0100

    created a notFound compound for wrong routes

--- github_finder/src/App.js
+++ github_finder/src/App.js
@@ -4,10 +4,11 @@ import Navbar from "./components/layout/Navbar";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
 import "./App.css";
-import Home from "./components/pages/Home";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
